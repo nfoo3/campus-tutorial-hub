d180638fadaaeaf5ca44cbcb44b4f2814b4b9da8
nfoo3 <niles.foo@gmail.com>
2026-04-07 15:41:53 -0400

    Add suggestion form link at bottom of page

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,18 @@ export default function Home() {
       {/* Search + resource categories */}
       <SearchableContent />
 
+      {/* Suggestion link */}
+      <div className="mx-auto max-w-5xl px-6 pb-12 text-center">
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScTuhOo3WJjIv10WSNWs3aPX8zjQFXoap0hC8S_PUA4n1KYkw/viewform?usp=sharing&ouid=116602849973708478319"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm text-gray-500 hover:text-[#4f50ff] hover:underline transition-colors"
+        >
+          Don&apos;t see what you&apos;re looking for? Click here to make suggestions.
+        </a>
+      </div>
+
       {/* Floating back-to-top button */}
       <BackToTop />
     </>
